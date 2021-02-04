f840e150306635ff64eb2570579a2d9576d89f53
Ben Hayes <b.t.hayes@comcast.net>
2021-02-04 23:52:02 +0000

    begin CLI structure

--- lib/dog_breed_info/cli.rb
+++ lib/dog_breed_info/cli.rb
@@ -1,7 +1,26 @@
 class DogBreedInfo::CLI 
   
   def call 
+    list_breeds
+    get_user_input
+    # show_breed_info
+  end 
+  
+  def list_breeds
     puts "Currently Featured Dog Breeds:"
+    puts "1. Golden Retriever"
+    puts "2. Labrador Retriever"
+    puts "3. Australian Shepherd"
+  end
+  
+  def get_user_input
+    puts "Please select a breed to get more information:"
+    input = gets.strip
+    
+  end 
+  
+  def show_breed_info
+    
   end 
   
 end 
---
+++ notes.md
@@ -0,0 +1,8 @@
+1. initialize gem/environment setup/sort file dependencies
+2. create CLI class
+  2a. create call method to structure CLI
+  2b. create necessary helper methods 
+3. create DogBreed class
+  3a. create necessary helper methods
+4. create Scraper class
+  4a. create necessary helper methods 
