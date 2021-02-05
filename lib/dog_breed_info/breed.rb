d4cab92aabf41e175988f6128ef4b194cacc8550
Ben Hayes <b.t.hayes@comcast.net>
2021-02-05 20:35:04 +0000

    setup breed methods

--- lib/dog_breed_info/breed.rb
+++ lib/dog_breed_info/breed.rb
@@ -1,4 +1,19 @@
 class DogBreedInfo::Breed 
   attr_accessor :name, :group, :height, :weight, :lifespan 
   
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    save 
+  end
+  
+  def save
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
 end 
