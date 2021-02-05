9e91e563be664a66283c2908dc49afce37ed4036
Ben Hayes <b.t.hayes@comcast.net>
2021-02-05 18:59:54 +0000

    CLI helper methods

--- lib/dog_breed_info/cli.rb
+++ lib/dog_breed_info/cli.rb
@@ -16,7 +16,18 @@ class DogBreedInfo::CLI
   def get_user_input
     puts "Please select a breed to get more information:"
     input = gets.strip
-    
+    if input == "1"
+      puts "More info on 1"
+      elsif input == "2"
+      puts "More info on 2"
+      elsif input == "3"
+      puts "More info on 3"
+    end
+    puts "To view another breed type 'view new' or type 'exit' to exit."
+    input_2 = gets.strip
+    if input_2 == "view new"
+      call 
+    end 
   end 
   
   def show_breed_info
