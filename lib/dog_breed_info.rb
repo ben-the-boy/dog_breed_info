0705fe49d0919ca534f4685a1aee5b1613b9fe3d
Ben Hayes <b.t.hayes@comcast.net>
2021-02-05 19:57:21 +0000

    Require relative update

--- lib/dog_breed_info.rb
+++ lib/dog_breed_info.rb
@@ -1,5 +1,7 @@
 require_relative "./dog_breed_info/version"
 require_relative "./dog_breed_info/cli"
+require_relative "./dog_breed_info/breed"
+require_relative "./dog_breed_info/scraper"
 
 module DogBreedInfo
   class Error < StandardError; end
