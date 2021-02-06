47505c60343b623e8326b13219ff4766884a1757
Ben Hayes <b.t.hayes@comcast.net>
2021-02-06 21:54:52 +0000

    Scrape_featured_breeds update

--- lib/dog_breed_info.rb
+++ lib/dog_breed_info.rb
@@ -3,6 +3,10 @@ require_relative "./dog_breed_info/cli"
 require_relative "./dog_breed_info/breed"
 require_relative "./dog_breed_info/scraper"
 
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
 module DogBreedInfo
   class Error < StandardError; end
   # Your code goes here...
--- lib/dog_breed_info/scraper.rb
+++ lib/dog_breed_info/scraper.rb
@@ -1,4 +1,14 @@
 class DogBreedInfo::Scraper
   
+  def self.scrape_featured_breeds
+    doc = Nokogiri::HTML(open("https://dogtime.com/dog-breeds"))
+    featured_breeds = doc.css("div.featured-breed-text-container")
+    breed_names = featured_breeds.css("a.featured-breed-list-item-title").text
+    featured_breeds.each do |breed|
+      name = breed.css("a.featured-breed-list-item-title").text
+      DogBreedInfo::Breed.new(name)
+    end 
+    binding.pry 
+  end 
   
 end 
